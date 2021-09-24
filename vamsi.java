d97e03a9fc0acf7e7e370ea9c4ec597d79eef224
Vasirocks1 <90172879+Vasirocks1@users.noreply.github.com>
2021-09-24 20:40:17 +0530

    Create vamsi.java

---
+++ vamsi.java
@@ -0,0 +1,25 @@
+class Calc
+{
+	public int add(int ... n) //variable length arguments(varags)
+	{
+		int sum=0;
+		for(int i:n)
+		{
+			sum = sum + i;
+		}
+		{
+			return sum;
+		}
+		
+	}
+}
+public class Varargs {
+
+	public static void main(String[] args)
+	{
+		Calc obj = new Calc();
+		System.out.println(obj.add(4,5,9,3,6,5));
+
+	}
+
+}
